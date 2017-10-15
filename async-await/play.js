f553e631014d55ff5ac7f07345b1bfdc0d4beb6a
Gareth Fuller <gareth.fuller@clark.de>
2017-10-15 21:56:50 +0200

    more playing

--- async-await/play.js
+++ async-await/play.js
@@ -14,7 +14,7 @@ async function fetchAvatarUrl(userID) {
   // is then evavluated
   const response = await fetch(`https://catappapi.herokuapp.com/users/${userID}`)
   // if this was a promise we could not call the json like this
-  data = await response.json()
+  const data = await response.json()
   // now we can access data as we waitin for response
   return data.imageUrl
 }
@@ -22,5 +22,29 @@ async function fetchAvatarUrl(userID) {
 const result = fetchAvatarUrl(123)
 
 result
+
+// IMPORTANT
+// - async functions always return promises
+// - when using multiple promises, still better to use promise.all for example
+// to make sure they can all run at the same time
+
+// example of mixing ascyn and promise.all
+async function all() {
+  const user = 123
+  const userRequest = await fetch(`https://catappapi.herokuapp.com/users/${user}`)
+  const userData = await userRequest.json()
+
+  return await Promise.all(
+    // as async returns a promise we can add it to our new array of promises here
+    userData.cats.map(async function(catId) {
+      const response = await fetch(`https://catappapi.herokuapp.com/cats/${catId}`)
+      const data = await response.json()
+      return data.imageUrl
+    })
+  )
+}
+
+const data = all()
+data
 
 

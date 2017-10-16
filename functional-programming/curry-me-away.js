39874a22c0c508b4d133a48bfc6c51439aa4ef44
Gareth Fuller <gareth.fuller@clark.de>
2017-10-16 22:28:07 +0200

    some curry functions

---
+++ functional-programming/curry-me-away.js
@@ -0,0 +1,34 @@
+
+// function that has not been curried
+let dragon = (name, size, element) =>
+	name + ' is a ' +
+	size + ' dragon that breathes ' +
+	element + '!'
+
+const t = dragon('gareth', 'mighty', 'features');
+
+// CURRY version
+let dragoon =
+	name =>
+		size =>
+			element =>
+				name + ' is a ' +
+				size + ' dragon that breathes ' +
+				element + '!'
+
+const x = dragoon('gareth')('majestic')('fire')
+
+x
+
+// function can pass though the app and grab arguments as it goes,
+// gives you more flexibility
+
+// you can also curry via libs like lodash for example
+const _ = require('lodash')
+
+// THIS SMAZING LINE CONVERTS IT FOR YE, many libs also do this
+let dragine = _.curry(dragon)
+
+const y = dragine('lisa', 'lovely', 'hearts')
+
+y
